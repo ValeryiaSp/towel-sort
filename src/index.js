a47b76ef8c6b568cfe7be6920e8aee9a4c2dfefe
ValeryiaSp <valeryia.spitsyna@LeverX.local>
2022-03-04 00:24:56 +0300

    feat: implemented task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  } 
+  const result = [];
+    for (let i = 0; i < matrix.length; i++) {
+      let start = i % 2 ? matrix[i].length -1 : 0;
+      let step = i % 2 ? -1 : 1;
+      for (let j = start; j < matrix[i].length && j >=0; j = j + step){
+      result.push(matrix[i][j]);
+    }
+  }
+  return result;
 }
